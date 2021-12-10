7bb2614e5823af2eb88aa272460781049d880eb2
RahulP0369 <rp98514360@gmail.com>
2021-12-11 03:44:43 +0530

    Accept number of rows and number of columns from user and display  below pattern.

---
+++ Assignment10.1.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  Pattern()  
+//
+// Description   :   Accept number of rows and number of columns 
+//                   from user and display below pattern
+//
+// Input         :  Integer, Integer
+//
+// Output        :  Special Character
+//
+// Author        :  Rahul Patil
+//
+////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0,j = 0;
+
+    for (i = 1; i <= iRow; i++)
+    {
+        for (j = 0; j <= iCol; j++)
+        {
+            printf("\t*");
+        }
+        printf("\n");
+    }
+    
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+    printf("Enter number of rows");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of column");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
