79c09b51250897a610ce4affea4bbdf5be77e59d
RahulP0369 <rp98514360@gmail.com>
2021-12-14 20:20:31 +0530

    Add files via upload

---
+++ Assignment22.5.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+//////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  ChkVowel()
+//
+// Description   :  Write a program which accept string from user and display it in 
+//                  reverse order.
+//
+// Input         :  String
+//
+// Output        :  String
+//
+// Author        :  Rahul Patil
+//
+//////////////////////////////////////////////////////////////////////////////////////
+
+void Reverse(char *str)
+{
+    char *end = str;
+    char temp;
+
+    while (*end != '\0')
+    {
+        end++;
+    }
+    end--;
+
+    while (str < end)
+    {
+        temp = *str;
+        *str = *end;
+        *end = temp;
+
+        str++;
+        end--;
+    }
+    
+}
+
+int main()
+{
+    char arr[20];
+
+    printf("Enter string: \n");
+    scanf("%[^'\n']s",arr);
+
+    Reverse(arr);
+
+    printf("Modified string is: %s\n");
+
+    return 0;
+}
