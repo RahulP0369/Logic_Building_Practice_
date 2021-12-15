c14680ec509a5fa56aee5e1d2f147ac170845b4d
RahulP0369 <rp98514360@gmail.com>
2021-12-16 01:49:05 +0530

    program which checks whether 7th & 15th & 21st , 28th bit  is On or OFF.

---
+++ Assignment27.3.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+//////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name :  ChkBit()
+//
+// Description   :  Write a program which checks whether 7th & 15th & 21st , 28th bit 
+//                  is On or OFF.
+//
+// Input         :  Integer
+//
+// Output        :  Integer
+//
+// Author        :  Rahul Patil
+//
+//////////////////////////////////////////////////////////////////////////////////////
+
+typedef int BOOL;
+typedef unsigned int UINT;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkBit(UINT iNo)
+{
+    UINT iMask = 0x08104040;
+    UINT iResult = 0;
+    iResult = iNo & iMask;
+    if (iResult == iMask)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }   
+}
+
+int main()
+{
+    UINT iValue = 0;
+    BOOL bRet = FALSE;
+    printf("%d",&iValue);
+
+    bRet = ChkBit(iValue);
+    if (bRet == TRUE)
+    {
+        printf("15th bit is ON\n");
+    }
+    else
+    {
+        printf("15th bit is OFF");
+    }
+    return 0;
+}
